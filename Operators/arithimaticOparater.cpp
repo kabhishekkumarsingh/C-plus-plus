1d1d117c01005ae536abe30ce24550aa445c5e7e
Abhishek kumar singh <115399497+mailabhishekkumarsingh@users.noreply.github.com>
2023-10-16 06:04:27 +0530

    Create arithimaticOparater.cpp
    use of arithimatic oparater in c++

---
+++ Operators/arithimaticOparater.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter two number: ";
+    cin>>num1>>num2;
+    cout<<"sum of number1 and number2 is "<<num1+num2<<endl;
+    cout<<"substration of number1 and number2 is "<<num1-num2<<endl;
+    cout<<"multiplication of number1 and number2 is "<<num1*num2<<endl;
+    cout<<"division of number1 and number2 is "<<num1/num2;
+    return 0;
+}
