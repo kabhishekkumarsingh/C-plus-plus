bd06dd01707d98ffb6ed74dd32cfcd1d17186f1e
Abhishek kumar singh <115399497+mailabhishekkumarsingh@users.noreply.github.com>
2023-10-16 06:12:43 +0530

    Create welcomeClass.cpp
    welcome message using class

---
+++ Class and Object/welcomeClass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+class welcome{
+    public:
+    void display(){
+        cout<<"welcome to c++ Programing";
+    }
+};
+
+int main()
+{
+    welcome a;
+    a.display();
+    return 0;
+}
